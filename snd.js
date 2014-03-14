01d4a2f1d9383cc4e369d558142e5f75ba952030
Ravi Tejasvi <ravitejasvi@gmail.com>
2014-03-14 10:09:39 +0530

    Initial part of select 'n' display functions. Not complete yet.

---
+++ snd.js
@@ -0,0 +1,34 @@
+function select_word(dictionary_of_words){
+	/*
+		Selects a word from the dictionary which is not being used to print on the screen.
+		The printing/ display is done by the function display_word()
+	*/
+	var r = Math.random()
+	var word_index = r*(dictionary_of_words.length-1);
+	if(dictionary_of_words[word_index][0] == 'NU'){
+		dictionary_of_words[word_index][0] = 'CFD';		//Set as CFD here. The display function will
+														//Take care when to set it to ST. Or reset to 
+														//Not Used. 
+		return dictionary_of_words[word_index];
+	}
+	else{
+		select_word(dictionary_of_words);
+	}
+}
+
+function display_word(word){
+	/*
+	**	Displays the word on canvas
+		May use only canvas. So in that case, this function will just take care when 
+		to update the values of keys in the dictionary_of_words.
+	*/
+
+}
+
+//For testing only. TODO: Remove this code block.{
+var a = [{key:'ToBe', value: 'NU'},{ key: 'Current', value :'CFD'}, {key:'Used', value:'ST'} ];
+a.push({key:'test', value:'NU'});
+//	}
+console.log(a[0]);
+console.log(a[0][0]);
+//console.log(select_word(a));
