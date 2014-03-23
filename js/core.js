7ee2c4c81343d82a73b4e3c2e55d66d0075d3432
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2014-03-23 17:54:27 +0530

    EaselJS integration complete
    EaselJS integration done. Now, we should start deciding on different
    modes and levels and their difficulty levels and speeds of words falling
    down.

--- js/core.js
+++ js/core.js
@@ -1,28 +1,30 @@
 var canvasWidth = window.screen.availWidth;
 var canvasHeight = window.screen.availHeight;
 var canvas = 0;
-var context = 0;
 
 function initCanvas() {
-	canvas = document.getElementById("gameCanvas");
-	context = canvas.getContext("2d");
-	canvas.width = canvasWidth;
-	canvas.height = canvasHeight;
+	canvas = new createjs.Stage("gameCanvas");
+	document.getElementById("gameCanvas").width = canvasWidth;
+	document.getElementById("gameCanvas").height = canvasHeight;
 }
 
 function startWords() {
-	setInterval("startWord(); context.clearRect(0, 0, canvasWidth, canvasHeight);", 100);
+	setInterval("startWord();", 4000);
 }
 
 function startWord() {
-	context.font = "175% Helvetica";
 	var thisWord = getWord();
-	var thisX = canvasWidth/15 + ((Math.random() * canvasWidth) * 0.8);
-	var thisY = 0;
+	var text = new createjs.Text(thisWord, "200% Segoe UI", "black");
+	text.textBaseline = "alphabetic";
+	var thisX = canvasWidth / 15 + ((Math.random() * canvasWidth) * 0.8);
+	text.x = thisX;
+	text.y = 0;
+	canvas.addChild(text);
+	canvas.update();
 	var t = setInterval(function() {
-		context.fillText(thisWord, thisX, thisY);
-		thisY += 10;
-	}, 100);
+		text.y += 1;
+		canvas.update();
+	}, 10);
 
 }
 
