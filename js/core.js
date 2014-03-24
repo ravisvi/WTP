8174f70a93763f25308730fe7827b180bca49466
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2014-03-24 20:23:53 +0530

    Better quality of words falling down
    Next TODO:
    Call success_typed() and not_succ_typed() successfully

--- js/core.js
+++ js/core.js
@@ -1,6 +1,8 @@
 var canvasWidth = window.screen.availWidth;
 var canvasHeight = window.screen.availHeight;
 var canvas = 0;
+var speed = 50;
+var t1 = 0;
 
 function initCanvas() {
 	canvas = new createjs.Stage("gameCanvas");
@@ -9,26 +11,39 @@ function initCanvas() {
 }
 
 function startWords() {
-	setInterval("startWord();", 4000);
+	startWord();
+	setInterval(function() {
+		startWord();
+	}, 2000);
+	var t = setInterval(function() {
+		if (speed >= 0) {
+			speed -= 5;
+		} else if (speed < 0) {
+			speed += 6;
+		}
+		;
+		console.log(speed);
+		//clearInterval(t1);
+	}, 2000);
 }
 
 function startWord() {
-	var thisWord = getWord();
-	var text = new createjs.Text(thisWord, "200% Segoe UI", "black");
+	var thisWord = select_word();
+	var text = new createjs.Text(thisWord, "250% Segoe UI", "black");
 	text.textBaseline = "alphabetic";
 	var thisX = canvasWidth / 15 + ((Math.random() * canvasWidth) * 0.8);
 	text.x = thisX;
 	text.y = 0;
 	canvas.addChild(text);
 	canvas.update();
-	var t = setInterval(function() {
+	t1 = setInterval(function() {
 		text.y += 1;
 		canvas.update();
-	}, 10);
-
+	}, speed);
 }
 
-function getWord() {
-	var dummyWords = new Array("this", "is", "sparta", "hello", "world", "joker", "narendra", "ravi", "wtp", "kickass", "jolly", "madam", "it seems", "muhaha", "weka", "");
-	return dummyWords[Math.floor(Math.random() * 15)];
-}
+/*
+ function getWord() {
+ var dummyWords = new Array("this", "is", "sparta", "hello", "world", "joker", "narendra", "ravi", "wtp", "kickass", "jolly", "madam", "it seems", "muhaha", "weka", "");
+ return dummyWords[Math.floor(Math.random() * 15)];
+ }*/
