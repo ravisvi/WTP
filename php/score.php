d660d9500d028b7dbfe7ca87b479b95fe83c6353
Ravi Tejasvi <ravitejasvi@gmail.com>
2014-04-22 10:16:47 +0530

    Some PHP changes

--- php/score.php
+++ php/score.php
@@ -1,21 +1,22 @@
-<?PHP 
- $name = $_POST['Name']; 
- $score = $_POST['Score'];
- $date = $_POST['Date_'];
- $game_type = $_POST['Game_Type']; 
- 
- $link = @mysql_connect("localhost", "root", ""); 
- if (mysql_errno()){ 
- exit("-2"); 
- } 
- mysql_select_db('wtp', $link); 
- if (mysql_errno()){ 
- exit("-3"); 
- } 
- $sql = "INSERT INTO highscores(Name, Score, Date_, Game_Type) VALUES('$name', $score, $date, '$game_type')"; 
- $result = mysql_query($sql, $link); 
- if (mysql_errno()){ 
- exit("-4"); 
+<?php 
+ $name = $_GET['Name']; 
+ $score = $_GET['Score'];
+ $date = $_GET['Date_'];
+ $game_type = $_GET['Game_Type']; 
+ function post_scores(){
+ 	$link = @mysql_connect("localhost", "root", ""); 
+ 	if (mysql_errno()){ 
+ 		exit("-2"); 
+ 	} 
+ 	mysql_select_db('wtp', $link); 
+ 	if (mysql_errno()){ 
+ 		exit("-3"); 
+ 	} 
+ 	$sql = "INSERT INTO highscores(Name, Score, Date_, Game_Type) VALUES('$name', $score, '$date', '$game_type')"; 
+ 	$result = mysql_query($sql, $link); 
+ 	if (mysql_errno()){ 
+ 		exit("-4"); 
+ 	}
  } 
  exit("1"); 
 ?> 
