46266e58bfa478a6a2931f7f5add558757f3cfe6
Narendra Nath Joshi <narendranathjoshi@gmail.com>
2014-04-22 08:51:35 +0530

    Lot of Work
    All explained

--- js/core.js
+++ js/core.js
@@ -1,8 +1,7 @@
 var canvasWidth = window.screen.availWidth;
 var canvasHeight = window.screen.availHeight;
 var canvas = 0;
-var speed = 50;
-var t1 = 0;
+var speed = 10;
 
 function initCanvas() {
 	canvas = new createjs.Stage("gameCanvas");
@@ -19,16 +18,6 @@ function startWords() {
 	setInterval(function() {
 		startWord();
 	}, 2000);
-	var t = setInterval(function() {
-		if (speed >= 0) {
-			speed -= 5;
-		} else if (speed < 0) {
-			speed += 6;
-		}
-		;
-		//console.log(speed);
-		//clearInterval(t1);
-	}, 2000);
 }
 
 function handleEvent() {
@@ -38,16 +27,12 @@ function handleEvent() {
 		var letter = String.fromCharCode(event.keyCode + 32);
 		if (letter == my_cur[i].charAt(0)) {
 			flag = true;
-			/*window.removeEventListener("keydown", function() {
-			 handleEvent();
-			 }, true);
-			 window.addEventListener("keydown", function() {
-			 finishWord(my_cur[i]);
-			 }, true);*/
-			var return = finishWord(my_cur[i]);
-			while () {
+			window.removeEventListener("keydown", handleEvent, true);
+			window.addEventListener("keydown", function() {
 				finishWord(my_cur[i]);
-			}
+			}, true);
+			finishWord(my_cur[i]);
+
 			my_cur[i] = my_cur[i].substr(1);
 			console.log(letter + " : " + my_cur[i]);
 		}
@@ -55,34 +40,89 @@ function handleEvent() {
 }
 
 function finishWord(curWord) {
-	//work here
+	//while (curWord.length > 0) {
+	console.log("here");
 	var letter = String.fromCharCode(event.keyCode + 32);
 	if (letter == curWord.charAt(0)) {
 		curWord = curWord.substr(1);
 		console.log(letter + " : " + curWord);
 	}
-	return curWord.length;
+	//}
 	//event listener household job
-	/*window.removeEventListener("keydown", function() {
-	 finishWord(my_cur[i]);
-	 }, true);
-	 window.addEventListener("keydown", function() {
-	 handleEvent();
-	 }, true);*/
+	window.removeEventListener("keydown", finishWord, true);
+	window.addEventListener("keydown", function() {
+		handleEvent();
+	}, true);
 }
 
 function startWord() {
 	var thisWord = select_word();
-	var text = new createjs.Text(thisWord, "250% Helvetica", "black");
+	var text = new createjs.Text(thisWord, (175 + (Math.random() * 150)) + "% Helvetica", "black");
 	text.textBaseline = "alphabetic";
-	var thisX = canvasWidth / 15 + ((Math.random() * canvasWidth) * 0.8);
+	var thisX = canvasWidth / 15 + ((Math.random() * canvasWidth) * 0.75);
 	text.x = thisX;
 	text.y = 0;
 	canvas.addChild(text);
 	canvas.update();
-	t1 = setInterval(function() {
+	var t1 = setInterval(function() {
 		text.y += 1;
 		canvas.update();
-	}, speed);
+		if (text.y > canvasHeight + 50) {
+			clearInterval(t1);
+		}
+	}, ((Math.random() * 5) + speed));
 }
 
+/* version 2 */
+
+function start() {
+	window.addEventListener("keydown", function() {
+		handleType();
+	}, true);
+
+	var cur_list;
+	var readonly_cur_list;
+	var t;
+	var fonts = new Array("Helvetica", "Book Antiqua", "Arial Black", "Comic Sans MS", "Copperplate Gothic Light", "Rage Italic", "Times New Roman", "Futura");
+
+	function init() {
+		t = setInterval(function() {
+			document.getElementById("timerval").innerHTML = parseInt(document.getElementById("timerval").innerHTML) + 1;
+		}, 1000);
+
+		cur_list = new Array();
+		readonly_cur_list = new Array();
+
+		for (var i = 0; i < Math.ceil(Math.random() * 6) + 2; i++) {
+			var thisWord = select_word();
+			cur_list[i] = thisWord;
+			readonly_cur_list[i] = thisWord;
+			document.getElementById("game").innerHTML += "<p id='" + thisWord + "' style='position: absolute; font-family: " + fonts[Math.floor(Math.random() * 8)] + ";font-size: " + (Math.random() * 75 + 100) + "% ;top: " + (10 + (Math.random() * 80)) + "%; left: " + (10 + (Math.random() * 80)) + "%;'>" + thisWord + "</p>";
+		};
+	}
+
+	function handleType() {
+		var letter = String.fromCharCode(event.keyCode + 32);
+		for (var i = 0; i < cur_list.length; i++) {
+			if (letter == cur_list[i].charAt(0)) {
+				cur_list[i] = cur_list[i].substr(1);
+				document.getElementById(readonly_cur_list[i]).innerHTML = cur_list[i];
+			}
+		};
+
+		var complete = true;
+		for (var i = 0; i < cur_list.length; i++) {
+			if (cur_list[i].length != 0) {
+				complete = false;
+			}
+		}
+
+		if (complete) {
+			clearInterval(t);
+			document.getElementById("score").innerHTML = "Score : " + document.getElementById("timerval").innerHTML;
+			init();
+		}
+	}
+
+	init();
+}
